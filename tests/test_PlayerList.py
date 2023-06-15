d41e33f0205f49f90f6dcbeb9c56517e8012410e
magnetrwn <rpolli.dev@tutanota.com>
2023-06-16 01:14:25 +0200

    Implementing tests for CI/CD (2).

--- tests/test_PlayerList.py
+++ tests/test_PlayerList.py
@@ -1,3 +1,7 @@
 """Run tests for the PlayerList class"""
 
 from ..NetHang import Player, PlayerList
+
+def test_PlayerList_always_true():
+    """Just for testing the testing lol"""
+    assert True
